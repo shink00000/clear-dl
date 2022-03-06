d14a739d23fb3f0d69d7979758b5a77dcc973f55
shink00000 <r82.y252@gmail.com>
2022-03-06 14:47:54 +0900

    [fix]

--- cleardl/components/schedulers/polynomial_scheduler.py
+++ cleardl/components/schedulers/polynomial_scheduler.py
@@ -3,5 +3,5 @@ from torch.optim.lr_scheduler import LambdaLR
 
 class PolynomialScheduler(LambdaLR):
     def __init__(self, optimizer, gamma: float, n_iterations: int):
-        def lr_lambda(iter): return (1 - min(iter, n_iterations) / n_iterations) ** gamma
+        def lr_lambda(iter): return (1 - min(iter, n_iterations-1) / n_iterations) ** gamma
         super().__init__(optimizer, lr_lambda)
